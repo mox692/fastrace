cc9fe73a82146b41c62df9ac354aa48adc5b775a
github-action-benchmark <github@users.noreply.github.com>
2025-06-01 17:16:12 +0000

    add Rust Benchmark (cargo) benchmark result for a9cc7e26138d2d3c672b08d602489480882f0a01

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748791523042,
+  "lastUpdate": 1748798172785,
   "repoUrl": "https://github.com/mox692/fastrace",
   "entries": {
     "Rust Benchmark": [
@@ -31,6 +31,62 @@ window.BENCHMARK_DATA = {
             "name": "compare/rt_trace/100",
             "value": 7859,
             "range": "± 1443",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "mox692",
+            "username": "mox692"
+          },
+          "committer": {
+            "name": "mox692",
+            "username": "mox692"
+          },
+          "id": "a9cc7e26138d2d3c672b08d602489480882f0a01",
+          "message": "Add CI bench",
+          "timestamp": "2025-06-01T13:06:03Z",
+          "url": "https://github.com/mox692/fastrace/pull/6/commits/a9cc7e26138d2d3c672b08d602489480882f0a01"
+        },
+        "date": 1748798171910,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "compare/fastrace/100",
+            "value": 92,
+            "range": "± 4",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "compare/rt_trace/100",
+            "value": 3540,
+            "range": "± 9",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "compare/fastrace/1000",
+            "value": 27521,
+            "range": "± 416",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "compare/rt_trace/1000",
+            "value": 35257,
+            "range": "± 55",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "compare/fastrace/10000",
+            "value": 274268,
+            "range": "± 8102",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "compare/rt_trace/10000",
+            "value": 352506,
+            "range": "± 490",
             "unit": "ns/iter"
           }
         ]
