4f00d7a526a45dd38e7ce3c5f407290e0fe340c1
github-action-benchmark <github@users.noreply.github.com>
2025-06-01 15:25:23 +0000

    add Rust Benchmark (cargo) benchmark result for 0a858f9bc27e7ce367aeda93cc73e4a2772bc62f

---
+++ dev/bench/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1748791523042,
+  "repoUrl": "https://github.com/mox692/fastrace",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "mox692",
+            "username": "mox692"
+          },
+          "committer": {
+            "name": "mox692",
+            "username": "mox692"
+          },
+          "id": "0a858f9bc27e7ce367aeda93cc73e4a2772bc62f",
+          "message": "Add CI bench",
+          "timestamp": "2025-06-01T13:06:03Z",
+          "url": "https://github.com/mox692/fastrace/pull/6/commits/0a858f9bc27e7ce367aeda93cc73e4a2772bc62f"
+        },
+        "date": 1748791522564,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "compare/fastrace/100",
+            "value": 92,
+            "range": "± 4",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "compare/rt_trace/100",
+            "value": 7859,
+            "range": "± 1443",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
