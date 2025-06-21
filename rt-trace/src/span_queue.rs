be1bb81bb4e8d16a08af6f578dc518bc20912903
木村 元幸 <s15255@ca-20031043.tail5325a9.ts.net>
2025-06-21 16:59:55 +0900

    remove unused code

--- rt-trace/src/span_queue.rs
+++ rt-trace/src/span_queue.rs
@@ -1,29 +1,12 @@
 use once_cell::sync::Lazy;
 use parking_lot::Mutex;
 
-use crate::{
-    backend::perfetto::thread_descriptor, command::Command, consumer::send_command, span::RawSpan,
-    thread_id::get, SHARD_NUM,
-};
-use std::{
-    cell::{Cell, RefCell},
-    rc::Rc,
-    sync::Arc,
-};
+use crate::{command::Command, consumer::send_command, span::RawSpan, thread_id::get, SHARD_NUM};
+use std::{cell::Cell, sync::Arc};
 
 pub(crate) const DEFAULT_BATCH_SIZE: usize = 16384 / 16;
 
 thread_local! {
-    static SPAN_QUEUE: Rc<RefCell<SpanQueue>> = {
-        let mut queue = SpanQueue::new();
-
-        // perfetto specific operation.
-        // TODO: Can we put this logic elsewhere?
-        queue.push(thread_descriptor());
-
-        Rc::new(RefCell::new(queue))
-    };
-
     pub(crate) static THREAD_INITIALIZED: Cell<bool> = Cell::new(false);
 }
 
